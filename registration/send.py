16a24e6f9f7ebf56aea968ba129dfe83510a7fa1
vadim <vadimadr@gmail.com>
2015-04-22 22:13:57 +0300

    Activation email text changed

--- registration/send.py
+++ registration/send.py
@@ -1,14 +1,14 @@
 import requests
-from HipstaChat.settings import key, sandbox,request_url,ACTIVATION_HOSTNAME, USE_SANDBOX_MAIL, SEND_ACTIVATION_EMAIL
+from HipstaChat.settings import key, sandbox,request_url,ACTIVATION_HOSTNAME,    SEND_ACTIVATION_EMAIL
 
 
 def send_invite(rec, act):
     requests.post(request_url, auth=('api', key), data={
-        'from': 'HipstaTeam <postmaster@sandbox50f958596328413ba57a57bfeb12eb27.mailgun.org>',
-        'to': (rec if not USE_SANDBOX_MAIL else 'HipstaTeam <hipsta.team@mail.ru>'),
-        'subject': 'Hello',
-        'text': 'Hello from HipstaTeam, we are happy to be visited by you. So, if you want to join us, just click here.\n' + \
-        '%s/%s' % (ACTIVATION_HOSTNAME, act) + '\n'\
-        'It was not you? Just don\'t click that and all\'ll be ok'
+        'from': 'HipstaChat <noreply@hipstachat.ru>',
+        'to': rec,
+        'subject': 'HipstaChat - подтверждение email',
+        'text': '''Здраствуйте, ваш email был указан при регистрации в HipstaChat
+Для подтверждения email адреса пройдите по ссылке. Если вы не регестрировались в HipstaChat - просто проигнорируйте данное письмо
+%s/accounts/activate/%s''' % (ACTIVATION_HOSTNAME, act)
     })
 
