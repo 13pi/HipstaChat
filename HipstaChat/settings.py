dc55e56bc07f7954d9a13889785d07ff7bdd65d4
vadim <vadimadr@gmail.com>
2015-04-23 05:40:30 +0300

    some template pre works

--- HipstaChat/settings.py
+++ HipstaChat/settings.py
@@ -46,6 +46,16 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+# # TEMPLATE_CONTEXT_PROCESSORS = (
+#     'django.core.context_processors.auth',
+#     'django.contrib.auth.context_processors.auth',
+#     'django.core.context_processors.debug',
+#     'django.core.context_processors.i18n',
+#     'django.core.context_processors.media',
+#     'django.core.context_processors.request',
+#
+# )
+
 ROOT_URLCONF = 'HipstaChat.urls'
 
 AUTH_USER_EMAIL_UNIQUE = True
