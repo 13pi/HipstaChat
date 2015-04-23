f2b2e2e8908788f03ab3cefc54fd4622f8a9ae17
vadim <vadimadr@gmail.com>
2015-04-23 08:31:56 +0300

    added login forms

--- HipstaChat/settings.py
+++ HipstaChat/settings.py
@@ -44,17 +44,16 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'HipstaChat.middleware.LoginFormMiddleware'
 )
 
-# # TEMPLATE_CONTEXT_PROCESSORS = (
-#     'django.core.context_processors.auth',
-#     'django.contrib.auth.context_processors.auth',
-#     'django.core.context_processors.debug',
-#     'django.core.context_processors.i18n',
-#     'django.core.context_processors.media',
-#     'django.core.context_processors.request',
-#
-# )
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request'
+)
 
 ROOT_URLCONF = 'HipstaChat.urls'
 
@@ -96,7 +95,7 @@ AUTH_USER_MODEL = 'HipstaChat.HCUser'
 
 # AUTHENTICATION_BACKENDS = (
 # 'HipstaChat.custom_auth.HCUserModelBackend',
-#     'django.contrib.auth.backends.ModelBackend'
+# 'django.contrib.auth.backends.ModelBackend'
 # )
 
 from .settings_local import *
